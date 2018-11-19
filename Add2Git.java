5481c963aba357100f36afe1c4d5e0be2db9f853
ZhongZhijia920 <41658741+ZhongZhijia920@users.noreply.github.com>
2018-11-19 21:36:14 +0800

    Add with my first time.

---
+++ Add2Git.java
@@ -0,0 +1,11 @@
+package reflect;
+
+/**
+ * HelloWOrld
+ * @create: 2018-11-19 21:32
+ **/
+public class Add2Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Github!");
+    }
+}
