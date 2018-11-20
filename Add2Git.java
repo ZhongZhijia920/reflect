3b20001ef1a9fd59ee7ec136953d6788e1b0a73d
ZhongZhijia920 <41658741+ZhongZhijia920@users.noreply.github.com>
2018-11-20 21:24:25 +0800

    add branch

--- Add2Git.java
+++ Add2Git.java
@@ -7,5 +7,6 @@ package reflect;
 public class Add2Git {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("branch£¡");
     }
 }
